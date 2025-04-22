3a2724ecd2d2f19bfc11222dca58b68200f6d6b3
TouheedFathima <touheedfathima30@gmail.com>
2025-04-22 11:17:10 +0530

    commit

--- app.py
+++ app.py
@@ -218,5 +218,6 @@ def home():
     return render_template("index.html")  # If profile is filled, show chatbot
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 4000))
-    app.run(host="0.0.0.0", port=port)    
+    app.run(debug=True, port=5000)
+
+    
